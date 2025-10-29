d165ebbf4ba108cfd479e2f71e8299cb7ff6c3d0
Bryan <bryan001700@gmail.com>
2025-10-28 23:47:23 -0400

    still missing last half of from scratch

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,18 +1,54 @@
 // Create callback functions for Higher-Order Functions:
-const logEachValue = () => { };
+const logEachValue = (arr) => {
+  if(arr.length <= 0){
+    return arr;
+  }
+  for(let i=0;i<arr.length;i++){
+    console.log(`Value: ${arr[i]}, index: ${i}.`)
+  }
+};
 
-const makePeopleHappy = () => { };
+const makePeopleHappy = (people) => {
+ for(let i=0;i<people.length;i++){
+  let currentMood = people[i];
+  currentMood.isHappy = !currentMood.isHappy
+ }
+ console.log(people)
+};
 
-const getEvenNumbers = () => { };
+const getEvenNumbers = (numbers) => { 
+  const arr1 = [];
+  for(let i=0;i<numbers.length;i++){
+    if(numbers[i]%2===0){
+      arr1.push(numbers[i])}
+  };
+  return arr1;
+};
 
-const doubleEveryNumber = () => { };
 
-const convertToBooleans = () => { };
+const doubleEveryNumber = (numbers) => {
+  const doubled = numbers.map((num)=>{
+    return num * 2;
+  });
+  return doubled;
+ };
+
+const convertToBooleans = (arr) => { 
+ const convert = arr.map((el)=>{
+  return Boolean(el);
+ });
+ return convert;
+};
+
 
 // Create your own Higher Order Functions:
-const myForEach = () => { };
+const myForEach = (arr, callback) => { 
+  for(const value of arr){
+    callback(value)
+  }
+};
 
-const myMap = () => { };
+const myMap = (arr, tranform) => { };
 
 const myFilter = () => { };
 
